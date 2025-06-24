1e4059a304fc58d66685024da697adc9c7ff85c5
Piotr Kira <piotr@kira.pl>
2025-06-24 13:03:21 +0200

    Improve performance when using SQlite database
    Enable WAL mode for SQlite connection to improve perfromance, especially
    for distributed nature of Reforged Inbox.

--- app/database.py
+++ app/database.py
@@ -1,13 +1,19 @@
 import redis
-from sqlmodel import create_engine
+from sqlmodel import create_engine, text
 
 from app.config import AppConfig
 
 connect_args = {}
-if AppConfig.database.connection_url.startswith("sqlite:///"):
+is_sqlite = AppConfig.database.connection_url.startswith("sqlite://")
+if is_sqlite:
     connect_args["check_same_thread"] = False
 
 engine = create_engine(AppConfig.database.connection_url, echo=False, connect_args=connect_args)
+
+if is_sqlite:
+    with engine.connect() as conn:
+        conn.execute(text("PRAGMA journal_mode=WAL;"))
+        conn.commit()
 
 redis_pool = redis.ConnectionPool(
     host=AppConfig.redis.host, password=AppConfig.redis.password, port=AppConfig.redis.port, db=AppConfig.redis.db
